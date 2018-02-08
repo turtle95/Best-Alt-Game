af392c1cdd6faeac8d8c770d143d7af1405d814e
sean6258 <srlmysticmail@gmail.com>
2018-02-07 22:23:46 -0700

    Menu, Base UI, Transitions, Enemy Tracker

--- Guardian/Assets/Scripts/EnemyHealth.cs
+++ Guardian/Assets/Scripts/EnemyHealth.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour {
 
+    public variableTracker varTrack;
 
 	//kills the enemy when a rock hits it
 	void OnCollisionEnter(Collision col){
-		if (col.gameObject.CompareTag ("Rock"))
+        if (col.gameObject.CompareTag("Rock"))
+            varTrack = GameObject.Find("variableTracker").GetComponent<variableTracker>();
+            varTrack.EnemiesKilled += 1;
 			Destroy (this.gameObject);
 	}
 }
---
+++ Guardian/Assets/Scripts/variableTracker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class variableTracker : MonoBehaviour {
+
+
+    public int EnemiesKilled = 0;       //Some basic things we may want to track
+    public int CurrentStage = 1;
+    public int PlayerHP = 0;
+    public Text KilledNumber;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this); //So that variableTracker will always be able to track variables (stays inbetween scenes)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            KilledNumber = GameObject.Find("KilledNumber").GetComponent<Text>();
+            KilledNumber.text = EnemiesKilled.ToString();       //Updates UI
+        }
+		if (EnemiesKilled >= 10)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                SceneManager.LoadScene(2);      //Loads Level 2
+                CurrentStage = 2;
+            }
+        }
+
+        if (EnemiesKilled >= 30)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                SceneManager.LoadScene(3);      //Loads Level 3
+                CurrentStage = 3;
+            }
+        }
+
+        if (EnemiesKilled >= 70)
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                //SceneManager.LoadScene(4);    //Loads credits/end
+                CurrentStage = 4;
+            }
+        }
+    }
+}

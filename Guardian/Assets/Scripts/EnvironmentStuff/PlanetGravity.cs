5901b2dc5faad6fae1651debf457d5e521e551ef
Joshua Watts <joshuawatts3@gmail.com>
2018-02-21 15:40:07 -0700

    Gravity Works with movement on Stage 3!!!

---
+++ Guardian/Assets/Scripts/EnvironmentStuff/PlanetGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity : MonoBehaviour {
+
+	public float gravity = -10;
+
+	public void Attract(Transform turdNugget)
+	{
+		Vector3 gravityUp = (turdNugget.position - transform.position).normalized;
+		Vector3 turdsUp = turdNugget.up;
+
+		turdNugget.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+		Quaternion targetRotation = Quaternion.FromToRotation (turdsUp, gravityUp) * turdNugget.rotation;
+		turdNugget.rotation = Quaternion.Slerp (turdNugget.rotation, targetRotation, 50 * Time.deltaTime);
+	}
+}
---
+++ Guardian/Assets/Scripts/MainPlayerStuff/Stage3/Stage3Movement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage3Movement : MonoBehaviour {
+
+	public Transform mover;
+	public Transform planet;
+	public float walkSpeed = 10;
+	public float gravity = -10;
+	//public float distToGrounded = 5f; //the distance from player's origin to the ground when grounded
+	//GameObject checkground;
+	Transform player;
+	public CameraController camScript;
+
+	Vector3 movement;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> (); //assigns rb to the player's rigidbody
+		player = transform;
+	}
+		
+
+	void Update () {
+		
+		/* Commented out the check ground stuff becuase we might need it later, don't need it for anything right now though
+		bool fall;
+		if (Grounded ())
+			fall = false;
+		else
+			fall = true;
+		*/
+
+
+		//rotates the player based on its relation to the planet, applies gravity
+		WorldGravity();
+		
+		//creates a Vector3 out of the input Axis's
+		movement = new Vector3 ( Input.GetAxis ("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		//moves the player without directly adjusting its velocity, allows gravity to keep working
+		rb.MovePosition (rb.position + mover.TransformDirection(movement * walkSpeed * Time.deltaTime));
+
+
+		//rotates the items parented to the main player container based on mouse movement
+		mover.localRotation = Quaternion.Euler (mover.localRotation.x, camScript.mouseX, mover.localRotation.z);
+
+	}
+
+
+	public void WorldGravity()
+	{
+		//gets the distance between player and planet, essentially this is the direction you want to be facing
+		Vector3 gravityUp = (transform.position - planet.position).normalized;
+		//the up direction for the player
+		Vector3 turdsUp = transform.up;
+
+		//applies gravity based on the player's local down
+		rb.AddForce(turdsUp * gravity);
+
+		//creates a rotation between the gravityUp and the player's current up
+		Quaternion targetRotation = Quaternion.FromToRotation (turdsUp, gravityUp) * transform.rotation;
+	
+		//smoothly rotates the player to the target rotation
+		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, 50 * Time.deltaTime);
+	}
+
+	/*
+	bool Grounded(){
+		return Physics.Raycast (transform.position, -transform.up, distToGrounded);
+	}*/
+
+
+}

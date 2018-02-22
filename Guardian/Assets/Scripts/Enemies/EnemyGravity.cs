2bbc1a72502848d9833ce09bbb88cb0d4a072f55
Joshua Watts <joshuawatts3@gmail.com>
2018-02-21 23:40:32 -0700

    Improved Enemy Spawner
    No longer spawn on top of eachother, also stops spawning when all spawnpoints are full. Made the enemies rotate to face the ground for the planet stage

---
+++ Guardian/Assets/Scripts/Enemies/EnemyGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGravity : MonoBehaviour {
+
+
+
+	// Rotates the enemy to stand on the planet
+	void Start ()
+	{
+		GameObject planet = GameObject.FindGameObjectWithTag ("Planet");
+
+		Vector3 gravityUp = (transform.position - planet.transform.position).normalized;
+
+		transform.rotation = Quaternion.FromToRotation (transform.up, gravityUp) * transform.rotation;
+	}
+
+}
--- Guardian/Assets/Scripts/Enemies/EnemySpawn.cs
+++ Guardian/Assets/Scripts/Enemies/EnemySpawn.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour {
 
-	public Transform [] points; //array of spawnpoints
+	GameObject [] points; //array of spawnpoints
 	public GameObject enemy; //enemy prefab to spawn
+	GameObject [] spawnedEnemys; //array to store where all the enemies in the scene are
 
+	public float spawnTime = 3f;
 
 	void Start () //starts the spawn coroutine 
 	{
 		StartCoroutine (SpawnStuffs ());
-
+		points = GameObject.FindGameObjectsWithTag ("EnemySpawner");
 		//used to fill the map with enemies at the start but we probably don't want that to happen right away
 		//for (int i = 0; i < points.Length; i++) {
 		//	Instantiate (enemy, points [i].position, points [i].rotation);
@@ -19,10 +21,33 @@ public class EnemySpawn : MonoBehaviour {
 	}
 	
 	//waits a second, then chooses a random spawnpoint and spawns an enemy there, after that it calls itself again
-	IEnumerator SpawnStuffs(){
-		yield return new WaitForSeconds (1f);
-		int j = Random.Range (0, points.Length);
-		Instantiate (enemy, points [j].position, points [j].rotation);
+	IEnumerator SpawnStuffs()
+	{
+		yield return new WaitForSeconds (spawnTime);
+		spawnedEnemys = GameObject.FindGameObjectsWithTag ("Enemy");
+
+		if (spawnedEnemys.Length < points.Length) 
+		{
+			int j = Random.Range (0, points.Length);
+
+			bool doAgain;
+			do {
+				doAgain = false;
+				for (int i = 0; i < spawnedEnemys.Length; i++) {
+					if (Vector3.Distance (points [j].transform.position, spawnedEnemys [i].transform.position) < 1) { 
+						j = Random.Range (0, points.Length);
+						doAgain = true;
+						i = spawnedEnemys.Length;
+						Debug.Log ("Deflected");
+					} 
+				}
+			} while(doAgain);
+				
+			Instantiate (enemy, points [j].transform.position, points [j].transform.rotation);
+		}
+
 		StartCoroutine (SpawnStuffs ());
 	}
+
+
 }

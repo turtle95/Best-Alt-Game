9ff95e72a5c395a1cdd72a17f4b983e68711a19e
Joshua Watts <joshuawatts3@gmail.com>
2018-04-21 03:21:05 -0600

    Added minimap to stage 2/coded in the enemy icon

---
+++ Guardian/Assets/Scripts/EnvironmentStuff/DisableFogForMinimap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableFogForMinimap : MonoBehaviour {
+
+	bool revertFogState = false;
+
+	void OnPreRender(){
+		revertFogState = RenderSettings.fog;
+		RenderSettings.fog = enabled;
+	}
+
+	void OnPostRender(){
+		RenderSettings.fog = revertFogState;
+	}
+
+}

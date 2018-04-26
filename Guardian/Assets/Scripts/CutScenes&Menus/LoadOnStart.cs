a9d8727e560f87bd0967fac39facbf916697c710
Joshua Watts <joshuawatts3@gmail.com>
2018-04-26 17:37:23 -0600

    ...more last minute changes

--- Guardian/Assets/Scripts/CutScenes&Menus/LoadOnStart.cs
+++ Guardian/Assets/Scripts/CutScenes&Menus/LoadOnStart.cs
@@ -17,7 +17,7 @@ public class LoadOnStart : MonoBehaviour {
 	IEnumerator SplashScreen(){
 		yield return new WaitForSeconds (2);
         credits.text = "Josh - Project Lead, Programming\nJake - 3D Modelling\nAndrew - Terrain, Audio\nMarissa - Programming, Audio\nTanner - 3D Modelling\nSharon - Programming";
-		textss.text = "You Should be Doing Something.";
+		//textss.text = "You Should be Doing Something.";
 		yield return new WaitForSeconds (3);
 		vScript.CurrentStage = 1;
 		SceneManager.LoadScene(1);

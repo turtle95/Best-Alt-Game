fa4d48b2ce630f41377ff45dc2490df36b7caa53
Joshua Watts <joshuawatts3@gmail.com>
2018-05-06 11:22:41 -0600

    Finished Rough draft of new cut scene

---
+++ Guardian/Assets/Scripts/CutScenes&Menus/MiniFog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniFog : MonoBehaviour {
+
+	public GameObject fogStuff;
+	public GameObject fogPoof;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Rock")) 
+			{
+			fogStuff.SetActive(false);
+			fogPoof.SetActive (true);
+			StartCoroutine (ReSpawn ());
+			}
+	}
+
+	IEnumerator ReSpawn()
+	{
+		yield return new WaitForSeconds (1);
+		fogPoof.SetActive (false);
+		fogStuff.SetActive (true);
+	}
+}
---
+++ Guardian/Assets/Scripts/CutScenes&Menus/ShootGhost.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShootGhost : MonoBehaviour {
+
+	public NavMeshAgent navi;
+	private int destination = 0;
+	public Transform [] waypoints;
+
+	int prevDest =0;
+	int currDest = 0;
+
+
+
+	public AudioSource audManager;
+	public AudioClip dashSound;
+
+
+	public GameObject bullet;
+	GameObject rockUsedHere;
+
+	Rigidbody rb; 
+	public float launchSpeed = 100; 
+
+
+	// Use this for initialization
+	void Start () {
+		audManager = gameObject.GetComponent<AudioSource> ();
+		//StartCoroutine (Dash ());
+	}
+
+
+	void SetDestinations()
+	{
+		if (waypoints.Length == 0)
+			return;
+		Shoot ();
+		navi.destination = waypoints [destination].position;
+
+		destination = Random.Range (0, waypoints.Length);
+		while (destination == prevDest || destination == currDest) {
+			destination = Random.Range (0, waypoints.Length);
+		}
+
+		prevDest = currDest;
+		currDest = destination;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (!navi.pathPending && navi.remainingDistance < 7f)
+			SetDestinations ();
+
+
+
+	}
+
+	void Shoot(){
+		audManager.PlayOneShot(dashSound);
+		rockUsedHere = Instantiate (bullet, transform.position, transform.rotation);
+		rb = rockUsedHere.GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * launchSpeed;
+	}
+}

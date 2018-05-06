19bf226d08db3b372b01dc0fee1640bc3e99b027
sharonx <sharonhiahia@gmail.com>
2018-05-06 14:59:35 -0600

    added surrounding enemy to player lvl2

---
+++ Guardian/Assets/Scripts/Enemies/ChangeSpawnGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSpawnGroup : MonoBehaviour {
+    public GameObject group1;
+    public GameObject group2;
+    public variableTracker varTrack;
+
+
+	// Use this for initialization
+	void Start () {
+        varTrack = GameObject.Find ("variableTracker").GetComponent<variableTracker> ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (varTrack.EnemiesKilled == 15)
+        {
+            group1.SetActive(false);
+            group2.SetActive(true);
+        }
+	}
+}
